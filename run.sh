0b11c3fcab35ecba0c95ea5b34cea1786e84f8ce
Crying-Soul <grebnevegor2000@mail.ru>
2023-10-13 16:56:49 +0300

    Colorful test header

--- run.sh
+++ run.sh
@@ -6,10 +6,11 @@ gcc -o lab-3.out main.c
 if [ $? -eq 0 ]; then
     while IFS= read -r line; do
         if [[ $line == "#"* ]]; then
-            echo "Running test: $line"
+            echo -e "\033[31mRunning test: $line\033[0m"
         else
             echo "$line" | ./lab-3.out
         fi
+	sleep 1.5
     done < tests
 else
     echo "Ошибка компиляции программы."
