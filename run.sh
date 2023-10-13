48d91d375e2e143605d5111566786d3834810852
Crying-Soul <grebnevegor2000@mail.ru>
2023-10-13 13:46:49 +0300

    add multiple tests

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+
+
 gcc -o lab-3.out main.c
-./lab-3.out < tests
+
+# Проверяем успешность компиляции
+if [ $? -eq 0 ]; then
+    # Запускаем программу с разными тестами из файла tests
+    while IFS= read -r line; do
+        if [[ $line == "#"* ]]; then
+            # Начало нового теста, выводим название теста
+            echo "Running test: $line"
+        else
+            # Запускаем программу с текущим тестовым входом
+            echo "$line" | ./lab-3.out
+        fi
+    done < tests
+else
+    echo "Ошибка компиляции программы."
+fi
+
+# Удаляем скомпилированный файл после завершения тестов
+rm -f lab-3.out
+
